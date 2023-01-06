6e8f15be478a090f449fa33c53c623bd724c6265
medvinski <103368186+medvinski@users.noreply.github.com>
2023-01-06 20:30:06 +0100

    v3final

---
+++ ArmchairTreasureHunt/Data/Migrations/20230106182049_mssql.local_migration_180.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArmchairTreasureHunt.Data.Migrations
+{
+    public partial class mssqllocal_migration_180 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
